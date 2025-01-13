a3743748619253679cf5a84668ef5ddaa91b87ab
Umar Solanki <uysolanki@gmail.com>
2025-01-13 19:33:44 +0530

    day 2

---
+++ src/day2/sorting/SelectionSort.java
@@ -0,0 +1,31 @@
+package day2.sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		int arr[]= {6,7,2,5,3};
+		System.out.println("Before : "+Arrays.toString(arr));
+		selectionSort(arr);
+		System.out.println("After : "+Arrays.toString(arr));
+	}
+
+	private static void selectionSort(int[] brr) {
+		int temp;
+		for(int i=0;i<brr.length;i++) //01234
+		{
+		    for(int j=i+1;j<brr.length;j++) //1234,234,34,4
+		      {
+		    		if(brr[i]>brr[j])
+		    		{
+		    			temp=brr[i];
+		    			brr[i]=brr[j];
+		    			brr[j]=temp;
+		    		}
+		      }
+		}
+		
+	}
+
+}
