5cfe1f7bff63f79d94df13eba971b4a1cbaaf2ab
Umar Solanki <uysolanki@gmail.com>
2025-01-28 20:01:15 +0530

    day 11

--- src/day10/SinglyLinkedList.java
+++ src/day10/SinglyLinkedList.java
@@ -6,9 +6,64 @@ public class SinglyLinkedList {
 
 	public static void main(String[] args) {
 		Node head=null;
+		Scanner sc=new Scanner(System.in);
 		head=create(head);
+		System.out.println("Display After Creation");
+		display(head);
+		
+		head=addFirst(head);
+		System.out.println("Display After Adding At First Position");
+		display(head);
+		
+		addLast(head);
+		System.out.println("Display After Adding At Last Position");
+		display(head);
+		
+		System.out.println("Enter the position of new node");
+		int position=sc.nextInt();
+		addMid(head,position);
+		System.out.println("Display After Adding At " + position +" Position");
 		display(head);
 
+	}
+
+	private static void addMid(Node head, int position) {
+		Node temp=head;
+		int i=1;
+		while(i<position-1)  //3<3
+		{
+			temp=temp.next;
+			i++;
+		}
+		
+		Node nn=getNode();
+		nn.next=temp.next;
+		temp.next=nn;
+	}
+
+	private static void addLast(Node head) {
+		Node temp=head;
+		while(temp.next!=null)
+			temp=temp.next;
+		
+		Node nn=getNode();
+		temp.next=nn;
+		
+	}
+
+	private static Node addFirst(Node head) {
+		Node nn=getNode();
+		nn.next=head;
+		head=nn;
+		return head;
+	}
+
+	private static Node getNode() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Data for Node");
+		int info=sc.nextInt();
+		Node newNode=new Node(info);
+		return newNode;
 	}
 
 	private static void display(Node head) {
@@ -18,7 +73,7 @@ public class SinglyLinkedList {
 			System.out.print(temp.data + "->");
 			temp=temp.next;
 		}
-		System.out.print("NULL");
+		System.out.print("NULL\n");
 	}
 	//10->20->30->NULL
 	private static Node create(Node head) {
